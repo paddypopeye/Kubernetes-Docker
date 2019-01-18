d497abe813c6adb13dd0eacec1dbe9013d714ff4
popeye <paddypopeye@hotmail.com>
2019-01-18 18:20:42 +0100

    modified:   deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
-docker login -u "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t paddypopeye/multi-client:latest -t paddypopeye/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t paddypopeye/multi-server:latest -t paddypopeye/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t paddypopeye/multi-worker:latest -t paddypopeye/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker tag multi-client:latest paddypopeye/multi-client:latest
 docker push paddypopeye/multi-client:latest
 docker tag multi-client:$SHA paddypopeye/multi-client:$SHA
@@ -17,4 +18,4 @@ docker push paddypopeye/multi-worker:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=paddypopeye/multi-server:$SHA
 kubectl set image deployments/client-deployment client=paddypopeye/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=paddypopeye/multi-worker:$SHA
+kubectl set image deployments/worker-deployment worker=paddypopeye/multi-worker:$SHAes
