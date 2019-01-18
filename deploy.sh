83b9c94032104f535931397fc5611c8b186d1714
popeye <paddypopeye@hotmail.com>
2019-01-18 18:59:31 +0100

    modified:   deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-docker build -t paddypopeye/multi-client:latest -t paddypopeye/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t paddypopeye/multi-server:latest -t paddypopeye/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t paddypopeye/multi-worker:latest -t paddypopeye/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-echo "dockerdocker001" | sudo docker login -u "paddypopeye" --password-stdin
-docker push paddypopeye/multi-client:latest
-docker push paddypopeye/multi-client:$SHA
-docker push paddypopeye/multi-server:latest
-docker push paddypopeye/multi-server:$SHA
-docker push paddypopeye/multi-worker:latest
-docker push paddypopeye/multi-worker:$SHA
+sudo docker build -t paddypopeye/multi-client:latest -t paddypopeye/multi-client:$SHA -f ./client/Dockerfile ./client
+sudo docker build -t paddypopeye/multi-server:latest -t paddypopeye/multi-server:$SHA -f ./server/Dockerfile ./server
+sudo docker build -t paddypopeye/multi-worker:latest -t paddypopeye/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+echo "$DOCKER_PASSWORD" | sudo docker login -u "$DOCKER_USERNAME" --password-stdin
+sudo docker push paddypopeye/multi-client:latest
+sudo docker push paddypopeye/multi-client:$SHA
+sudo docker push paddypopeye/multi-server:latest
+sudo docker push paddypopeye/multi-server:$SHA
+sudo docker push paddypopeye/multi-worker:latest
+sudo docker push paddypopeye/multi-worker:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=paddypopeye/multi-server:$SHA
 kubectl set image deployments/client-deployment client=paddypopeye/multi-client:$SHA
